9014d55ddf1ae989500f2a514589066af82f117e
Diarassouba Mamadou <diara@ndaya.com>
2026-02-19 12:52:55 +0000

    ok

--- src/components/layout/WrapperContent.tsx
+++
@@ -1,9 +0,0 @@
-interface WrapperProps {
-  children: React.ReactNode;
-}
-
-const Wrapper: React.FC<WrapperProps> = ({ children }) => {
-  return <div>{children}</div>;
-};
-
-export default Wrapper;
